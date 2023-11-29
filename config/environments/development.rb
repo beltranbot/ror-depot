ca7d30b83846a1c981151c167d733abc873efbd1
beltranbot <beltranbot@gmail.com>
2023-11-28 21:32:59 -0500

    using webhooks and channels

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # allow domains other tha localhost for action_cable
+  config.action_cable.disable_request_forgery_protection = true
 end
