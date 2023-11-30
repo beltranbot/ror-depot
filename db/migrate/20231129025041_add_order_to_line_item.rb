79e6055cb6b2368cfb8aec96f679a5cc7a680023
beltranbot <beltranbot@gmail.com>
2023-11-29 19:47:00 -0500

    added create order functionality

---
+++ db/migrate/20231129025041_add_order_to_line_item.rb
@@ -0,0 +1,12 @@
+class AddOrderToLineItem < ActiveRecord::Migration[7.1]
+  def up
+    add_reference :line_items, :order, null: true, foreign_key: true
+    change_column :line_items, :cart_id, :integer, null: true
+  end
+
+  def down
+    change_column :line_items, :cart_id, :integer, null: false
+    remove_reference :line_items, :order, null: true, foreign_key: true
+  end
+
+end
